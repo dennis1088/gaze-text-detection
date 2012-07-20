2dcfb052a9fd03aa5154021c84ef2387b317e206
Dennis Rodriguez <dennis1088@gmail.com>
2012-07-20 16:47:38 -0400

    created an image of current status of letter extraction.  made new function for extracting letters from a labeled swt image, saved variables for testing function in the examples.mat file

---
+++ extractletters.m
@@ -0,0 +1,54 @@
+function [ letters ] = extractletters( swtMap, swtLabel, ccNum )
+%LETTERCANDIDATES Summary of this function goes here
+%   Detailed explanation goes here
+
+numLetters = 0;
+letters = zeros(size(swtLabel));
+maxLetterHeight = 300;
+minLetterHeight = 10;
+
+for i=1:ccNum
+    
+    [r,c] = find(swtLabel==i);
+    idx = sub2ind(size(swtMap),r,c);
+    componentSW = swtMap(idx);
+    varianceSW = var(componentSW);
+    meanSW = mean(componentSW);
+    width = max(c) - min(c);
+    height = max(r) - min(r);
+    aspectRatio = width/height;
+    diameter = sqrt(width^2+height^2);
+    medianSW = median(componentSW);
+    maxSW = max(componentSW);
+    
+    % Accepted font heights are between 10px and 300px
+    if height>maxLetterHeight | height<minLetterHeight, continue, end
+    
+    % Reject CC with hight stroke width variance
+    if varianceSW/meanSW > 1.5, continue, end
+    
+    % Ratio between the diameter of the connected component and its
+    % median stroke width to be a value less than 10
+    if diameter/medianSW >= 10, continue, end
+    
+    % Aspect ratio to be a value between 0.1 and 10
+    if aspectRatio < 0.1 && aspectRatio > 10, continue, end
+    
+    if size(componentSW,1)/maxSW < 5, continue, end
+    
+    if width > height*2.5, continue, end
+    
+    
+    letters(idx) = 1;
+    
+    %     if varianceSW <= meanSW*0.5
+    %         %if 0.1 <= aspectRatio && aspectRatio <= 10
+    %             numLetters = numLetters + 1;
+    %             letters(idx) = 1;
+    %         %end
+    %     end
+    
+end
+
+end
+
